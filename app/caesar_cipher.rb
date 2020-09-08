7100f5ab24b55f1f11e6f496a61fda39440a072c
LilTits <titouan@guiochet.com>
2020-09-08 20:30:18 +0200

    Start implemanting the logic

--- app/caesar_cipher.rb
+++ app/caesar_cipher.rb
@@ -2,5 +2,11 @@
 
 # Implementation of the ceasar code
 class CaesarCipher
-  def caesar_cipher(message, shift); end
+  ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z].freeze
+  # MAJ_ALPHABET = ALPHABET.upcase
+  def caesar_cipher(message, _shift)
+    split_array = message.split(//)
+    split_array.each do |char|
+    end
+  end
 end
