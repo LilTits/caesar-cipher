70e96fde2e4ef66d9d0d0f7b66608be90646befe
LilTits <titouan@guiochet.com>
2020-09-09 10:12:03 +0200

    Update the logic to change the letter

--- app/caesar_cipher.rb
+++ app/caesar_cipher.rb
@@ -3,10 +3,19 @@
 # Implementation of the ceasar code
 class CaesarCipher
   ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z].freeze
-  # MAJ_ALPHABET = ALPHABET.upcase
-  def caesar_cipher(message, _shift)
+
+  def caesar_cipher(message, shift)
     split_array = message.split(//)
     split_array.each do |char|
+      if char == ' '
+        puts 'space'
+      elsif ALPHABET.include?(char)
+        puts ALPHABET.index(char) + shift
+      elsif ALPHABET.include?(char.downcase)
+        puts ALPHABET.index(char.downcase) + shift
+      else
+        puts 'idk'
+      end
     end
   end
 end
