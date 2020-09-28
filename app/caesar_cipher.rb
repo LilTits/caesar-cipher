5c423b5c13726c99a0d8509c2f6a444b32949ee3
Titouan <titouan@guiochet.com>
2020-09-28 17:55:18 +0200

    Improving the overall syntaxe

--- app/caesar_cipher.rb
+++ app/caesar_cipher.rb
@@ -3,23 +3,24 @@
 # Implementation of the ceasar code
 class CaesarCipher
   ALPHABET = ('a'..'z').to_a
+  MAJ_ALPHABET = ('A'..'Z').to_a
 
   def caesar_cipher(message, shift)
     split_array = message.split(//)
     new_message = []
+
     split_array.each do |char|
       if char == ' '
         new_message << ' '
       elsif ALPHABET.include?(char)
-        new_letter = ALPHABET.index(char) + shift
-        new_message << ALPHABET[new_letter]
-      elsif ALPHABET.include?(char.downcase)
-        new_letter = ALPHABET.index(char.downcase) + shift
-        new_message << ALPHABET[new_letter].upcase
-      else
-        puts 'idk'
+        new_letter_position = ALPHABET.index(char) + shift
+        new_message << ALPHABET[new_letter_position]
+      elsif MAJ_ALPHABET.include?(char)
+        new_letter_position = MAJ_ALPHABET.index(char) + shift
+        new_message << MAJ_ALPHABET[new_letter_position]
       end
     end
+
     new_message.join
   end
 end
