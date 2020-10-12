e99aadb584c490cdfc08f354bace5357503cd26b
LilTits <titouan@guiochet.com>
2020-10-12 10:16:24 +0200

    Refactoring and extraction into a private method

--- app/caesar_cipher.rb
+++ app/caesar_cipher.rb
@@ -6,47 +6,34 @@ class CaesarCipher
   MAJ_ALPHABET = ('A'..'Z').to_a
 
   def encode(message, shift)
-    split_array = message.split(//)
+    split_message = split_text(message)
+
+    shift_key(split_message, shift)
+  end
+
+  def decode(message, unshift)
+    split_message = split_text(message)
+
+    shift_key(split_message, unshift)
+  end
+
+  private
+
+  def split_text(split_message)
+    split_message.split(//)
+  end
+
+  def shift_key(split_message, shift)
     new_message = []
 
-    split_array.each do |char|
+    split_message.each do |char|
       if ALPHABET.include?(char)
         new_letter_position = ALPHABET.index(char) + shift
-        if ALPHABET.length < new_letter_position
-          new_letter_position = new_letter_position - ALPHABET.length        
-        end
+        new_letter_position -= ALPHABET.length if ALPHABET.length < new_letter_position
         new_message << ALPHABET[new_letter_position]
       elsif MAJ_ALPHABET.include?(char)
         new_letter_position = MAJ_ALPHABET.index(char) + shift
-        if MAJ_ALPHABET.length < new_letter_position
-          number = new_letter_position - ALPHABET.length        
-        end
-        new_message << MAJ_ALPHABET[new_letter_position]
-      else
-        new_message << char
-      end
-    end
-
-    new_message.join
-  end
-
-  def decode(message, shift)
-    split_array = message.split(//)
-    new_message = []
-    unshift = -shift
-    
-    split_array.each do |char|
-      if ALPHABET.include?(char)
-        new_letter_position = ALPHABET.index(char) + unshift
-        if ALPHABET.length < new_letter_position
-          new_letter_position = new_letter_position - ALPHABET.length        
-        end
-        new_message << ALPHABET[new_letter_position]
-      elsif MAJ_ALPHABET.include?(char)
-        new_letter_position = MAJ_ALPHABET.index(char) + unshift
-        if MAJ_ALPHABET.length < new_letter_position
-          new_letter_position = new_letter_position - ALPHABET.length        
-        end
+        new_letter_position - ALPHABET.length if MAJ_ALPHABET.length < new_letter_position
         new_message << MAJ_ALPHABET[new_letter_position]
       else
         new_message << char
@@ -56,3 +43,7 @@ class CaesarCipher
     new_message.join
   end
 end
+
+# jules = CaesarCipher.new
+# message = jules.decode('oh kdfkdjh yrxv shuphwwudlw gdvvxuhu od frqilghqwldolwh vlpsohphqw', 3)
+# puts message
